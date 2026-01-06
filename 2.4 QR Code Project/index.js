24e37a4c6963744291726693d9cf43bb6d1f4253
Tanjimul-Adnan <tanjimuladnan@hotmail.com>
2026-01-06 14:52:17 +0600

    qr code project added

---
+++ 2.4 QR Code Project/index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs"; 
+inquirer
+  .prompt([
+    {
+    message:"Type in tour URL",
+    name : "URL"
+    }
+  ])
+  .then((answers) => {
+    const url = answers.URL; 
+    const qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qrcode.png"));
+    console.log("QR code generated and saved as qrcode.png");
+
+     fs.writeFile("URL.txt", url, (err) => {
+          if (err) throw err;
+          console.log("The file has been saved!");
+        });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
